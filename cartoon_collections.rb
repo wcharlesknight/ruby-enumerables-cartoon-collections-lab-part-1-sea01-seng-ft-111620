ba83d3d68049235614e3965017263b8265a2b270
Charlie Knight <williamchknight@gmail.com>
2020-11-07 17:28:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
- array.each_with_index do {|character, index|
+ array.each_with_index do |character, index|
    puts "#{index} #{character}"
   end
 end
